8fe2ce055dedd9e10007906998c724ae12da71ea
Marshall Conover <marzhall.o+github@gmail.com>
2016-08-21 00:57:21 -0400

    Create a better testing interface for the code.

--- dataDeduplicator.py
+++ dataDeduplicator.py
@@ -1,14 +1,15 @@
 import math
+import sys
 
 dataset = [["IBM", "0691197987", "contact@ibm.com", "www.ibm.com"],
     ["Heroku", "4519080374", "feedback@heroku.com", "signup.heroku.com"],
     ["Dockers", "3072134041", "contact@dockers.com", "www.dockers.com"],
-    ["Docker", "6356809766", "contact@docker.com", "www.docker.com"],
     ["ANSYS", "1374023052", "", "www.ansys.com"],
     ["ANSYS, Inc.", "1374023052", "contact@ansys.com", "www.ansys.com"],
     ["The New York Times", "1848312818", "nytnews@nytimes.com", "www.nytimes.com"],
     ["The Gaurdian", "5139764161", "us-hr@gaurdian.com", "www.thegaurdian.com"],
     ["International Business Machines", "0691197987", "contact@ibm.com", "www.ibmserver.com"],
+    ["Docker", "6356809766", "contact@docker.com", "www.docker.com"],
     ["The Huffington Post", "6230388160", "scoop@huffingtonpost.com", "www.huffingtonpost.com"],
     ["AMD", "7079013926", "corporate.query@amd.com", "www.amd.com"],
     ["NVIDIA", "1830157005", "info@nvidia.com", "www.nvidia.com"],
@@ -88,21 +89,22 @@ def splitByNonAlphaNumeric(word): # will produce something like ["www", "ansys",
 def getMostSimilarToIndividualRecord(record, listOfOtherRecords, wordFrequencies, averageDistance):
     allComparisons = [zip(record, other) for other in listOfOtherRecords]
     levDistances = []
+    results = dict()
     for comparison in allComparisons:
         comparisonDistance = averageDistancePerFieldHelper(comparison, wordFrequencies) 
-        print "for " + str(comparison) + "distance is" + str(comparisonDistance)
+        #print "for " + str(comparison) + "distance is" + str(comparisonDistance)
         levDistances.append(comparisonDistance)
-    print "ayy"
-    for levDist in levDistances:
-        print "For " + str(levDist) +  " weight average is " + str(math.floor(weightClosenessByAverage(levDist, averageDistance)*100))
-    return sorted(levDistances, key=lambda x: int(math.floor((weightClosenessByAverage(x, averageDistance))*100)))
+        results[str(comparisonDistance)] = [otherRecord for (rec, otherRecord) in comparison]
+    #for levDist in levDistances:
+        # print "For " + str(levDist) +  " weight average is " + str(math.floor(weightClosenessByAverage(levDist, averageDistance)*100))
+    return [results[str(value)] for value in (sorted(levDistances, key=lambda x: int(math.floor((weightClosenessByAverage(x, averageDistance))*100))))]
 
 def weightClosenessByAverage(comparison, averageComparison):
     overallWeight = 0
     for field in zip(comparison, averageComparison):
         value1, value2 = field
         differenceFromAverage = value1*abs(1.0/(100.0 if (value2-value1) == 0 else (value2 - value1)))
-        print str(value1) + " " + str(value2) + " " + str(value1 - value2) + " " + str(differenceFromAverage)
+        # print str(value1) + " " + str(value2) + " " + str(value1 - value2) + " " + str(differenceFromAverage)
         overallWeight = overallWeight + differenceFromAverage
 
     return overallWeight
@@ -110,11 +112,16 @@ def weightClosenessByAverage(comparison, averageComparison):
 def main():
     wordFrequencies = getWords(dataset)
     averageDistance = computeAverageDistancePerField(dataset)
-    print averageDistance
-    print getMostSimilarToIndividualRecord(dataset[4], dataset, wordFrequencies, averageDistance)
-
-
-
+    #print averageDistance
+    print "one"
+    while True:
+        print "Items in dataset are:"
+        for i in range(0, len(dataset)-1):
+            print str(i) +": " + str(dataset[i])
+        print "Select a value by its index to see the records fuzzily closest to it."
+        sys.stdout.flush()
+        x = int(input())
+        print getMostSimilarToIndividualRecord(dataset[x], dataset, wordFrequencies, averageDistance)
 
 if __name__ == "__main__":
     main()
